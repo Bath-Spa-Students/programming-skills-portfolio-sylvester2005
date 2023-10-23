86dc316ff082119c2e9020cbac6b4a07741101e2
sylvester2005 <sylvester14105@gmail.com>
2023-10-23 10:59:36 +0400

    Exercise4.py

---
+++ Chapter 3- Data Structures/Exercises/Exercise4.py
@@ -0,0 +1,6 @@
+# Create a list of people to invite to dinner
+invitees = ["Albert Einstein", "Marie Curie", "Leonardo da Vinci"]
+
+# Send dinner invitations to each person
+for person in invitees:
+    print("Dear " + person + ", you are cordially invited to dinner at my place.")
